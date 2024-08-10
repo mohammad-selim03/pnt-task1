fcbe28c22040239fd656f8f52a0c8b85dc8fafdb
mernpeoplentech <noor@peoplentech.com.bd>
2024-08-10 13:23:21 +0600

    final route

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,18 +1,9 @@
-import { bannerImageOne } from "@/assets";
+import { banner } from "@/constants";
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
   return NextResponse.json({
-    banner: {
-      _id: 1001,
-      priceText: "Starting at $999.90",
-      title: "The best tablet Collection 2024",
-      textOne: "Exclusive offer",
-      offerPrice: "-30%",
-      textTwo: "off this week",
-      buttonLink: "/products",
-      image: bannerImageOne,
-    },
+    banner: banner,
     status: "success",
     message: "Keep alive!",
   });
